99d37c63cc012cc80ece33f5831c62011b53e9b4
shihab <echoshihab@gmail.com>
2019-10-06 17:09:00 -0400

    protect add technologist form from invalid users

--- dimanager/frontend/src/actions/techs.js
+++ dimanager/frontend/src/actions/techs.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-import { createMessage } from "./messages";
+import { createMessage, returnErrors } from "./messages";
 
-import { GET_TECHS, DELETE_TECH, ADD_TECH, GET_ERRORS } from "./types";
+import { GET_TECHS, DELETE_TECH, ADD_TECH } from "./types";
 
 //GET TECHNOLOGISTS
 export const getTechs = () => dispatch => {
@@ -13,7 +13,9 @@ export const getTechs = () => dispatch => {
         payload: res.data
       });
     })
-    .catch(err => console.log(err));
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
 
 //ADD TECHNOLOGISTS
@@ -27,16 +29,9 @@ export const addTech = tech => dispatch => {
         payload: res.data
       });
     })
-    .catch(err => {
-      const errors = {
-        msg: err.response.data,
-        status: err.response.status
-      };
-      dispatch({
-        type: GET_ERRORS,
-        payload: errors
-      });
-    });
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
 
 //DELETE TECHNOLOGISTS
@@ -50,14 +45,7 @@ export const deleteTech = id => dispatch => {
         payload: id
       });
     })
-    .catch(err => {
-      const errors = {
-        msg: err.response.data,
-        status: err.response.status
-      };
-      dispatch({
-        type: GET_ERRORS,
-        payload: errors
-      });
-    });
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
--- dimanager/techs/api.py
+++ dimanager/techs/api.py
@@ -3,9 +3,16 @@ from rest_framework import viewsets, permissions
 from .serializers import TechSerializer
 
 class TechViewSet(viewsets.ModelViewSet):
-    queryset = Tech.objects.all()
+    # queryset = Tech.objects.all() this is to view all
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticated #changed from AllowAny
     ]
 
+    def get_queryset(self):
+        return self.request.user.leads.all()
+
     serializer_class = TechSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+        
