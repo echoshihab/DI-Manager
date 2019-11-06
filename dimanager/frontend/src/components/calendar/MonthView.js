6aac8585a8579a20fb12c41d2e65e5a956b02f67
shihab <echoshihab@gmail.com>
2019-11-05 21:07:08 -0500

    add and style month and year to monthview

--- dimanager/frontend/src/components/calendar/MonthView.js
+++ dimanager/frontend/src/components/calendar/MonthView.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+//months
+
 export class MonthView extends Component {
   constructor() {
     super();
@@ -20,9 +22,38 @@ export class MonthView extends Component {
 
   render() {
     const { isActive, day, month, year, daysInMonth } = this.state;
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
     return (
-      <div className="container mt-5">
+      <div className="container d-flex align-items-center flex-row justify-content-center h-100">
+        <div clasName="btn-group btn-group-vertical">
+          <button type="button" className="btn btn-sm btn-outline-primary">
+            +
+          </button>
+          <button type="button" className="btn btn-sm btn-outline-primary">
+            -
+          </button>
+        </div>
         <h1>{year}</h1>
+        <div className="btn-group" role="group" aria-label="months">
+          {months.map(month => (
+            <button type="button" key={month} className="btn btn-secondary">
+              {month}
+            </button>
+          ))}
+        </div>
       </div>
     );
   }
